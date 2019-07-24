a08e551ab1d2df17d9b43bc90d6dc6cdbdc986e5
Scott Penrose <scottp@dd.com.au>
2019-07-24 14:40:01 +1000

    Update tool fixes

--- tools/update-version.sh
+++ tools/update-version.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+echo "START"
 if [ -z "$(git status --porcelain)" ]; then
-  echo "Working on new release..."
+  echo "NOTE: Working on new release..."
 else
-  echo "Uncommitted GIT changes. Please start with a clean repository"
+  echo "ERROR: Uncommitted GIT changes. Please start with a clean repository"
   exit 1
 fi
 
+echo "STEP: Checkout latest version"
 git checkout master
+echo "STEP: Patch version (auto increment)"
 npm version patch
+echo "STEP: Commit package version changes"
 git add package.json
 git commit -m 'Auto update'
-echo "Pushing repository and tags"
+echo "STEP: Pushing repository and tags"
 git push
 git push --tags
 
