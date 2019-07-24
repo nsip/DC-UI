cf1dca8da85c24115b0b0ee48c40ceee9e6451d9
Scott Penrose <scottp@dd.com.au>
2019-07-24 14:36:44 +1000

    Update tool fixes

--- tools/update-version.sh
+++ tools/update-version.sh
@@ -3,10 +3,15 @@
 set -e
 
 if [ -z "$(git status --porcelain)" ]; then
-  echo "Working"
+  echo "Working on new release..."
 else
   echo "Uncommitted GIT changes. Please start with a clean repository"
   exit 1
 fi
 
-git checkout master && npm version patch && git add package.json && git commit -m 'Auto update' && git push && git push --tags"
+git checkout master
+npm version patch
+git add package.json
+git commit -m 'Auto update'
+git push
+git push --tags"
