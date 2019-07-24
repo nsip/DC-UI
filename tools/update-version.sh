d21976af082740f499e91803e248ad15c906d800
Scott Penrose <scottp@dd.com.au>
2019-07-24 14:35:57 +1000

    Updated package

---
+++ tools/update-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$(git status --porcelain)" ]; then
+  echo "Working"
+else
+  echo "Uncommitted GIT changes. Please start with a clean repository"
+  exit 1
+fi
+
+git checkout master && npm version patch && git add package.json && git commit -m 'Auto update' && git push && git push --tags"
