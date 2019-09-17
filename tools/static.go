1bcf99c9833b159d27ad4da31ab23c5b757f3ac7
Scott Penrose <scottp@dd.com.au>
2019-09-17 16:44:46 +1000

    Revert "Tools and bild"
    This reverts commit 5f845ae10c672da914702997bb05847fee7d90e6.

--- tools/static.go
+++ tools/static.go
@@ -1,9 +1,7 @@
 package main
 
 import "net/http"
-import "fmt"
 
 func main() {
-	fmt.Print("Static server started. http://localhost:8002/\n")
 	http.ListenAndServe(":8002", http.FileServer(http.Dir("./")))
 }
