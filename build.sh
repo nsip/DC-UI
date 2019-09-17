5f845ae10c672da914702997bb05847fee7d90e6
Scott Penrose <scottp@dd.com.au>
2019-09-17 15:29:01 +1000

    Tools and bild

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ CWD=`pwd`
 mkdir -p build
 mkdir -p build/Linux64/DC-UI
 mkdir -p build/Mac/DC-UI
-./node_modules/.bin/quasar build
-rsync -av dist/spa-mta/* build/Linux64/DC-UI/
-rsync -av dist/spa-mta/* build/Mac/DC-UI/
-rsync -av dist/spa-mta/* build/Win64/DC-UI/
+mkdir -p build/Win64/DC-UI
+#./node_modules/.bin/quasar build
+rsync -av dist/spa-mat/* build/Linux64/DC-UI/
+rsync -av dist/spa-mat/* build/Mac/DC-UI/
+rsync -av dist/spa-mat/* build/Win64/DC-UI/
 
 echo "Building Linux64 binaries..."
 GOOS=linux
@@ -21,7 +22,7 @@ LDFLAGS="-s -w"
 OUTPUT=$CWD/build/Linux64/DC-UI
 GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static tools/static.go
 
-echo "Building Windows64 binaries..."
+echo "Building Win64 binaries..."
 GOOS=windows
 GOARCH=amd64
 LDFLAGS="-s -w"
@@ -36,12 +37,12 @@ OUTPUT=$CWD/build/Mac/DC-UI
 GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static tools/static.go
 
 echo "ZIP Linux64"
-cd $CWD/build/Mac
-zip -qr ../DC-UI-Mac-$VERSION.zip *
+cd $CWD/build/Linux64
+zip -qr ../DC-UI-Linux64-$VERSION.zip *
 
-echo "ZIP Windows64"
-cd $CWD/build/Windows64
-zip -qr ../DC-UI-Windows64-$VERSION.zip *
+echo "ZIP Win64"
+cd $CWD/build/Win64
+zip -qr ../DC-UI-Win64-$VERSION.zip *
 
 echo "ZIP Mac"
 cd $CWD/build/Mac
--- tools/static.go
+++ tools/static.go
@@ -1,7 +1,9 @@
 package main
 
 import "net/http"
+import "fmt"
 
 func main() {
+	fmt.Print("Static server started. http://localhost:8002/\n")
 	http.ListenAndServe(":8002", http.FileServer(http.Dir("./")))
 }
