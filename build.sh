589133b7b39325905c7b848d324fb56561bce94f
Scott Penrose <scottp@dd.com.au>
2019-11-28 04:31:27 +1100

    Build script

--- build.sh
+++ build.sh
@@ -1,50 +1,17 @@
 #!/bin/bash
 
 set -e
+go get gopkg.in/cheggaaa/pb.v1
+cd tools; go build release.go; cd ..
 
-VERSION="v0.0.0"
+VERSION="$(./tools/release n3 version)"
 
 CWD=`pwd`
-
-mkdir -p build
-mkdir -p build/Linux64/DC-UI
-mkdir -p build/Mac/DC-UI
-mkdir -p build/Win64/DC-UI
+npm install
 ./node_modules/.bin/quasar build
-rsync -av src/assets dist/spa-mat/
-rsync -av dist/spa-mat/* build/Linux64/DC-UI/
-rsync -av dist/spa-mat/* build/Mac/DC-UI/
-rsync -av dist/spa-mat/* build/Win64/DC-UI/
 
-echo "Building Linux64 binaries..."
-GOOS=linux
-GOARCH=amd64
-LDFLAGS="-s -w"
-OUTPUT=$CWD/build/Linux64/DC-UI
-GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static tools/static.go
+cd dist/spa-mat/
+zip -qr ../../DC-UI-$VERSION.zip *
 
-echo "Building Win64 binaries..."
-GOOS=windows
-GOARCH=amd64
-LDFLAGS="-s -w"
-OUTPUT=$CWD/build/Win64/DC-UI
-GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static.exe tools/static.go
-
-echo "Building Macintosh binaries..."
-GOOS=darwin
-GOARCH=amd64
-LDFLAGS="-s -w"
-OUTPUT=$CWD/build/Mac/DC-UI
-GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static tools/static.go
-
-echo "ZIP Linux64"
-cd $CWD/build/Linux64
-zip -qr ../DC-UI-Linux64-$VERSION.zip *
-
-echo "ZIP Win64"
-cd $CWD/build/Win64
-zip -qr ../DC-UI-Win64-$VERSION.zip *
-
-echo "ZIP Mac"
-cd $CWD/build/Mac
-zip -qr ../DC-UI-Mac-$VERSION.zip *
+echo "If happy with zip, run the following release command"
+echo " ./tools/release DC-UI DC-UI.zip DC-UI-$VERSION.zip"
